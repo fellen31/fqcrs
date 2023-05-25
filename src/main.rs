1e6c0e7b31f75220d3fe6ed9a36b0198e0ea94be
Felix Lenner <felix.lenner@igp.uu.se>
2023-05-25 10:04:40 +0200

    Average qual reflects correct average error probability, as in NanoPlot

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
 extern crate needletail;
 
+use needletail::{parse_fastx_stdin};
 use std::str;
-use needletail::{parse_fastx_stdin, Sequence, FastxReader};
-use std::io;
 
 fn main() {
-    let filename = "..";
 
-    let mut n_bases = 0;
-    let mut n_valid_kmers = 0;
     let mut reader = parse_fastx_stdin().expect("valid path/file");
+    
     while let Some(record) = reader.next() {
         let seqrec = record.expect("invalid record");
-        // keep track of the total number of bases
-        n_bases += seqrec.num_bases();
-        let mut read_name = str::from_utf8(seqrec.id())
+        
+        let read_name = str::from_utf8(seqrec.id())
             .unwrap()
             .split_whitespace()
             .collect::<Vec<&str>>();
         
-        if let Some(read_name) =read_name.first() {
-            
+        if let Some(read_name) =read_name.first() {       
             let baseq = seqrec.qual().unwrap().iter().map(|b| *b as i32-33).collect::<Vec<_>>();
             
-            println!("{}\t{}\t{}", read_name, seqrec.num_bases(), average(&baseq));
+            println!("{}\t{}\t{}", read_name, seqrec.num_bases(), average_qual(&baseq));
 
         } 
     }
 }
 
-fn average(numbers: &[i32]) -> f32 {
-    numbers.iter().sum::<i32>() as f32 / numbers.len() as f32
+fn average_qual(quals: &[i32]) -> f32 {
+    let mut sum = 0 as f32; 
+    
+    for q in quals.iter() {
+        sum = sum + 10_f32.powf(*q as f32/-10 as f32);
+    }
+    
+    return -10_f32 * (sum / quals.len() as f32).log10(); 
 }
